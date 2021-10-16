59ecd57857b91a91004eead83a460001b90eb21c
TFofanova-py <tfofanova@gmail.com>
2021-10-16 18:27:35 +0300

    + 1 function. Adding logo on a picture.

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import telebot
 from telebot import types
+from PIL import Image
+import os
 
-bot = telebot.TeleBot("2085631034:AAFdxr3MiZ6gkkvtkdlSDxmI37ClVp0tAGg")
+token = "2085631034:AAFdxr3MiZ6gkkvtkdlSDxmI37ClVp0tAGg"
+bot = telebot.TeleBot(token)
 
 
 # напишем, что делать нашему боту при команде старт
 @bot.message_handler(commands=["start", "help"])
-def send_keyboard(message, text="Привет, я бот, который собирает информацию о продвижении дел на нашей стройке:)"):
+def send_keyboard(message, text="Привет, чем я могу тебе помочь?"):
     keyboard = types.ReplyKeyboardMarkup()  # наша клавиатура
-    itembtn1 = types.KeyboardButton("Ок, понятно")  # создадим кнопку
+    itembtn1 = types.KeyboardButton("Поместить логотип на картинку")  # создадим кнопку
     keyboard.add(itembtn1)  # добавим кнопку
 
     # пришлем это все сообщением и запишем выбранный вариант
-    msg = bot.send_message(message.from_user.id,
+    msg = bot.send_message(message.chat.id,
                            text=text, reply_markup=keyboard)
+
+    bot.register_next_step_handler(msg, callback_worker)
+
+
+def callback_worker(call):
+    if call.text == "Поместить логотип на картинку":
+        msg = bot.send_message(call.chat.id, 'Отлично, загрузите фото')
+        bot.register_next_step_handler(msg, add_logo)
+
+
+# добавление логотипа на картинку
+def add_logo(msg):
+    photo_id = msg.photo[-1].file_id
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open(f"pic{msg.from_user.id}.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    bot.send_message(msg.chat.id, "Спасибо, сейчас добавлю лого!")
+    logo_file = "logoColliers.jpg"
+    logo_im = Image.open(logo_file)
+    logo_width, logo_height = logo_im.size
+
+    im = Image.open(f"pic{msg.from_user.id}.jpg")
+    width, height = im.size
+    im.paste(logo_im, tuple([width - logo_width, height - logo_height, width, height]))
+    bot.send_photo(msg.chat.id, im)
+
+    os.remove(f"pic{msg.from_user.id}.jpg")
 
 
 @bot.message_handler(regexp=r"^#работы_")
